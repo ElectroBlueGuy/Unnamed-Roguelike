1f56b6612c2f1dd501a44d27d87ea89e673b3605
ElectroBlueGuy <salogninic@gmail.com>
2019-10-23 19:42:44 +0200

    aggiunto movimento diagonale, una libreria solo per le varie entita'

--- input_handlers.py
+++ input_handlers.py
@@ -10,6 +10,14 @@ def handle_keys(key):
             return {'move': (-1, 0)}
     elif key.vk == libtcod.KEY_KP6:
             return {'move': (1, 0)}
+    elif key.vk == libtcod.KEY_KP7:
+            return {'move': (-1, -1)}
+    elif key.vk == libtcod.KEY_KP9:
+            return {'move': (1, -1)}
+    elif key.vk == libtcod.KEY_KP1:
+            return {'move': (-1, 1)}
+    elif key.vk == libtcod.KEY_KP3:
+            return {'move': (1, 1)}
     
     if key.vk == libtcod.KEY_ENTER and libtcod.KEY_ALT:
         # Full screen
