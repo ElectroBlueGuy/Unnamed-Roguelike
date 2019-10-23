294cdf4ad07afcf3619113988d52aee600460b8b
ElectroBlueGuy <salogninic@gmail.com>
2019-10-23 20:14:32 +0200

    Added blockage, added map, added various functions to clean up the code

--- engine.py
+++ engine.py
@@ -2,34 +2,38 @@ import tcod as libtcod
 import tcod.event
 from entity import Entity
 from input_handlers import handle_keys
+from map_objects import GameMap
+from render_functions import clear_all, render_all
 
 def main():
     screen_width = 80
     screen_height = 50
+    map_width = 80
+    map_height = 45
     
+    colors = {
+        'dark_wall': libtcod.Color(0, 0, 100),
+        'dark_ground': libtcod.Color(50, 50, 150)
+    }
+
     player = Entity(int(screen_width / 2), int(screen_height / 2), '@', libtcod.white)
     npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), '@', libtcod.yellow)
     entities = [npc, player]
-
     libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
-
-    libtcod.console_init_root(screen_width, screen_height, 'Untitled Roguelike', False)
+    libtcod.console_init_root(screen_width, screen_height, 'Untitled Roguelike', False, tcod.RENDERER_SDL2)
     con = libtcod.console.Console(screen_width, screen_height)
+    game_map = GameMap(map_width, map_height)
     key = libtcod.Key()
     mouse =libtcod.Mouse()
 
     while not libtcod.console_is_window_closed():
         libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
 
-        libtcod.console_set_default_foreground(con, libtcod.white)
-        libtcod.console_put_char(con, player.x, player.y, '@', libtcod.BKGND_NONE)
-        libtcod.console_blit(con, 0, 0, screen_width, screen_height, 0, 0, 0)
-        libtcod.console_set_default_foreground(0, libtcod.white)
-        libtcod.console_put_char(0, player.x, player.y, '@', libtcod.BKGND_NONE)
+        render_all(con, entities, game_map, screen_width, screen_height, colors)
+
         libtcod.console_flush()
 
-        libtcod.console_put_char(con, player.x, player.y, ' ', libtcod.BKGND_NONE)
-        libtcod.console_put_char(0, player.x, player.y, ' ', libtcod.BKGND_NONE)
+        clear_all(con, entities)
     
         action = handle_keys(key)
 
@@ -39,7 +43,8 @@ def main():
 
         if move:
             dx, dy = move
-            player.move(dx, dy)
+            if not game_map.is_blocked(player.x + dx, player.y + dy):
+                player.move(dx, dy)
 
         if exit:
             return True
---
+++ map_objects/__init__.py
@@ -0,0 +1,2 @@
+from map_objects.game_map import GameMap
+from map_objects.tile import Tile
